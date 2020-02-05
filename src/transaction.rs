ec53a7ef620a4b329491aac58461b3a39c91367d
jliu158 <jliu158@illinois.edu>
2020-02-05 17:10:21 -0600

    implement transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,18 +1,47 @@
+extern crate rand;
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use ring::digest;
+use ring::signature::{self, Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Transaction {
+    pub input: Vec<TxIn>,
+    pub output: Vec<TxOut>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct TxIn {
+    pub previous_output: OutPoint,
+    pub script_sig: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct OutPoint {
+    pub txid: String,
+    pub index: u8,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct TxOut {
+    pub value: u64,
+    pub script_pubkey: String,
 }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    let m = bincode::serialize(&t).unwrap();
+    let txid = digest::digest(&digest::SHA256, digest::digest(&digest::SHA256, m.as_ref()).as_ref());
+    let sig = key.sign(txid.as_ref());
+    return sig;
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+    let m = bincode::serialize(&t).unwrap();
+    let txid = digest::digest(&digest::SHA256, digest::digest(&digest::SHA256, m.as_ref()).as_ref());
+    let public_key_ = signature::UnparsedPublicKey::new(&signature::ED25519, public_key.as_ref());
+    let ret = public_key_.verify(txid.as_ref(), signature.as_ref()).is_ok();
+    return ret;
 }
 
 #[cfg(any(test, test_utilities))]
@@ -21,8 +50,43 @@ mod tests {
     use crate::crypto::key_pair;
 
     pub fn generate_random_transaction() -> Transaction {
-        Default::default()
-        //unimplemented!()
+        use rand::Rng;
+        const CHARSET: &[u8] = b"0123456789abcdef";
+        const RIPEMD160_LEN: usize = 40;
+        const SHA256_LEN: usize = 64;
+        const SCRIPTSIG_LEN: usize = 144;
+        let mut rng = rand::thread_rng();
+
+        let txid: String = (0..SHA256_LEN)
+            .map(|_| {
+                let idx = rng.gen_range(0, CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
+        let index: u8 = rng.gen();
+        let out_point = OutPoint { txid: txid, index: index };
+
+        let pub_key: String = (0..RIPEMD160_LEN)
+            .map(|_| {
+                let idx = rng.gen_range(0, CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
+        let value: u64 = rng.gen();
+        let tx_out = TxOut { value: value, script_pubkey: pub_key };
+
+        let script_sig: String = (0..RIPEMD160_LEN)
+            .map(|_| {
+                let idx = rng.gen_range(0, CHARSET.len());
+                CHARSET[idx] as char
+            })
+            .collect();
+        let tx_in = TxIn { previous_output: out_point, script_sig: script_sig };
+
+        let mut inputs = vec![tx_in];
+        let mut outputs = vec![tx_out];
+        let tx = Transaction{ input: inputs, output: outputs };
+        return tx;
     }
 
     #[test]
