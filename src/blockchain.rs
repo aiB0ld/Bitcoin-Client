903679ea3afb915c817f55453eb8cb620e7092ef
jliu158 <jliu158@illinois.edu>
2020-03-27 09:33:18 -0400

    state

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -18,11 +18,7 @@ impl Blockchain {
         // bytes32[0] = 0;
         // bytes32[1] = 0;
         let mut bytes32 = [0u8; 32];
-        bytes32[3] = 255u8;
-        bytes32[4] = 255u8;
-        bytes32[5] = 255u8;
-        bytes32[6] = 255u8;
-        bytes32[7] = 255u8;
+        bytes32[2] = 1u8;
         let difficulty: H256 = bytes32.into();
         let timestamp = 0u128;
         let transactions = Vec::new();
